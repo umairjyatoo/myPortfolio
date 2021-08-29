5d7022af3f4ee94c7b363e8c073b409dbc7ea98d
umairjyatoo <umairjyatoo@gmail.com>
2021-08-29 18:56:19 +0530

    title issue resolved

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,24 +6,28 @@ $(document).ready(function () {
     $("#skill_scroll").fadeOut();
 
     $("#about").click(function () {
+        document.title = 'My Portfolio : About';
         $("#index").fadeOut();
         $("#about_scroll").fadeIn();
         $('#about_left').addClass('animated slideInLeft');
         $('#about_right').addClass('animated slideInRight');
     });
     $("#work").click(function () {
+        document.title = 'My Portfolio : Work';
         $("#index").fadeOut();
         $("#work_scroll").fadeIn();
         $('#work_left').addClass('animated slideInLeft');
         $('#work_right').addClass('animated slideInRight');
     });
     $("#skill").click(function () {
+        document.title = 'My Portfolio : Skill';
         $("#index").fadeOut();
         $("#skill_scroll").fadeIn();
         $('#skill_left').addClass('animated slideInLeft');
         $('#skill_right').addClass('animated slideInRight');
     });
     $("#contact").click(function () {
+        document.title = 'My Portfolio : Contact';
         $("#index").fadeOut();
         $("#contact_scroll").fadeIn();
         $('#contact_left').addClass('animated slideInLeft');
@@ -37,6 +41,18 @@ $(document).ready(function () {
         $('#index_right').addClass('animated slideInRight');
     });
 
+    $("#goBack").click(function () {
+        document.title = 'My Portfolio : Home';
+    });
+    $("#goBackContact").click(function () {
+        document.title = 'My Portfolio : Home';
+    });
+    $("#goBackSkill").click(function () {
+        document.title = 'My Portfolio : Home';
+    });
+    $("#goBackWork").click(function () {
+        document.title = 'My Portfolio : Home';
+    });
     //typed animation
     var typed = new Typed(".typing", {
         strings: ['a Software Engineer', 'an API Specialist', 'a Freelancer', 'a Lifelong learner'],
